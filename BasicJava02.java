9b812d5bb9a8c26084a0e8552d18031fa3f83f1c
madhan1999-maddy <madhanstjit1212@gmail.com>
2023-12-21 12:16:40 +0530

    02 Basic entry program

---
+++ BasicJava02.java
@@ -0,0 +1,9 @@
+public class BasicJava02 {
+    public static void main(String[] args) {
+        int a,b,c;
+        a=2;
+        b=4;
+        c=a+b;
+        System.out.println("c=" + c ) ;
+    }
+}
