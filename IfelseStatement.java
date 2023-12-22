b189d99cf6668a350e4e3821723802ef56f8bc1c
madhan1999-maddy <madhanstjit1212@gmail.com>
2023-12-22 09:30:29 +0530

    Conditions

---
+++ IfelseStatement.java
@@ -0,0 +1,20 @@
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class IfelseStatement {
+    public static void main(String[] args) {
+        Scanner sc= new Scanner(System.in);
+        int age=sc.nextInt();
+
+        if (age<=12){
+            System.out.println("Child");
+        }
+        else if(age>=12&&age<=18){
+            System.out.println("teenager");
+
+        }
+        else{
+            System.out.println("aduld");
+        }
+    }
+}
